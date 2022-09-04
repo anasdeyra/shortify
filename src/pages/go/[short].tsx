89bf0cf36465555e2325c3b278b43f39ef112338
Anass <45261478+anasdeyra@users.noreply.github.com>
2022-09-04 13:22:10 +0100

    added "go-to" endpoint

---
+++ src/pages/go/[short].tsx
@@ -0,0 +1,27 @@
+import { link } from "@prisma/client";
+import { GetServerSideProps } from "next";
+import React, { useEffect } from "react";
+
+export default function Short({ origin }: Props) {
+  useEffect(() => {
+    window.location.assign(origin);
+  });
+  return <></>;
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const short = ctx.query.short;
+  const link: link | null = await (
+    await fetch(
+      `${process.env.VERCEL_URL || "http://localhost:3000"}/api/go-to/${short}`
+    )
+  ).json();
+
+  return {
+    props: { origin: link?.origin },
+  };
+};
+
+type Props = {
+  origin: string;
+};
